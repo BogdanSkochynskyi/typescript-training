4b7eede1262001ffb58896faec328ff42ec1e468
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-11 11:47:00 +0200

    Simple types example

---
+++ 02-variables/sampe-types.js
@@ -0,0 +1,9 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Bogdan";
+var lastName = "Skochynsky";
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+//Use template strings
+console.log("Hi ".concat(firstName, " ").concat(lastName));
---
+++ 02-variables/sampe-types.ts
@@ -0,0 +1,12 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Bogdan";
+let lastName: string = "Skochynsky"
+
+console.log(found);
+console.log("The grade is " + grade);
+
+console.log("Hi " + firstName + " " + lastName);
+
+//Use template strings
+console.log(`Hi ${firstName} ${lastName}`);
